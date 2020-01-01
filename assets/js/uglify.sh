55c4e9d3ab7dc08b8d673b87a1f460c2a43c13a6
workstation.local <workstation@localworkstation.local>
2020-01-01 21:57:38 +0200

    Adding minified content

---
+++ assets/js/uglify.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+baseDir='/home/workstation/websites/webzona/webyzona.com/templates/themeforest/globalnews'
+projectDir=$baseDir'/assets/js'
+exportDir=$baseDir'/_site/assets/js'
+
+cd $exportDir && echo 'Removing everyting from directory:' $exportDir && rm *.js;
+cd $projectDir
+echo 'Currently in ' $projectDir;
+for file in *.js;
+do short=${file%.js};
+echo 'Writing to ' $exportDir/$short.js
+uglifyjs $file > $exportDir/$short.js
+done;
